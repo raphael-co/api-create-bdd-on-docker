be10bd1c833bce618709fc6ce6610eec316da73d
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-03-05 15:32:19 +0100

    cors update

--- backEnd/app.ts
+++ backEnd/app.ts
@@ -3,40 +3,36 @@ import helmet from 'helmet';
 import cors from 'cors';
 import dotenv from "dotenv"
 import routes from './src/routes/routes';
-// import * as http from 'http';
-// import { Server } from "socket.io";
 
 dotenv.config()
 
 const env: any = process.env
 
-
 console.log(env.PORT);
+console.log(`Ready on ${env.NODE_ENV} mode`)
 
-console.log(`Ready on ${env.NODE_ENV} mode`)//express
 const app: Application = express();
 
-app.use(cors({
-  origin: ['http://192.168.1.196:8080','http://raphael.netgraph.fr'], // Assurez-vous que le port correspond au port de votre front-end
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
-}));
+// Configuration CORS pour autoriser votre front-end
+const corsOptions = {
+  origin: ['http://192.168.1.196:8080', 'http://raphael.netgraph.fr', 'https://raphael.netgraph.fr'], // Ajoutez également la version HTTPS si nécessaire
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Assurez-vous d'inclure 'OPTIONS' pour les requêtes preflight
+  credentials: true, // Cela est nécessaire pour les cookies de session côté client
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
+app.use(cors(corsOptions));
 app.use(helmet());
-
 app.use(express.json());
-
 app.use('/', routes);
 
 app.get('/', async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send({
-      message: 'API is up and running',
-    });
+  return res.status(200).send({
+    message: 'API is up and running',
   });
+});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('running socket');
-  process.env.PORT ? console.log(`server : http://localhost:${env.PORT}/`) : console.log(`local : http://localhost:4000/`);
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
