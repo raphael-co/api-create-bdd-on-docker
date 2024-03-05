052ca1965f623f8f738cf1a21b78ba1a8dc09422
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-03-05 18:32:00 +0100

    Update app.ts

--- backEnd/app.ts
+++ backEnd/app.ts
@@ -21,7 +21,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use('/', routes);
