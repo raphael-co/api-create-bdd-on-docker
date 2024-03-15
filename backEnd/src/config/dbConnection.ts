7e4f050b570117601be8f58aebd3a2cf8065a627
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-03-15 11:00:32 +0100

    connect to docker bdd

--- backEnd/src/config/dbConnection.ts
+++ backEnd/src/config/dbConnection.ts
@@ -1,21 +1,21 @@
 import mysql from 'mysql2/promise';
 
 async function createDatabaseConnection() {
-  const connection = await mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'bddStokage',
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-  });
-
   // const connection = await mysql.createConnection({
-  //   host: 'mysql', // Utilisez localhost si le code s'exécute hors de Docker
-  //   // port: 3306, // Port mappé pour le service mysql
+  //   host: '127.0.0.1',
   //   user: 'root',
   //   password: 'root',
-  //   database: 'bddStokage'
+  //   database: 'bddStokage',
+  //   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
   // });
+
+  const connection = await mysql.createConnection({
+    host: 'mysql', // Utilisez localhost si le code s'exécute hors de Docker
+    // port: 3306, // Port mappé pour le service mysql
+    user: 'root',
+    password: 'root',
+    database: 'bddStokage'
+  });
   
 
   console.log('Database bddStokage is connected successfully!');
