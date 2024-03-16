92fcff0e0496bd1176ce8d297c19dff1fce3b006
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-03-16 18:56:49 +0100

    Update UserServices.ts

--- backEnd/src/services/UserServices/UserServices.ts
+++ backEnd/src/services/UserServices/UserServices.ts
@@ -112,7 +112,7 @@ export class UserServices {
                 };
             }
 
-            console.log(users);
+            console.log('users :', users);
             
             const user = users;
 
